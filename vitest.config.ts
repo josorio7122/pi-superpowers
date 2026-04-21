fe3a4515e30195430950f6ba68b027279c2fc766
josorio7122 <josorio7122@users.noreply.github.com>
2026-04-21 17:47:10 -0500

    chore(test): split e2e into vitest.e2e.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		passWithNoTests: true,
-		include: ["src/**/*.test.ts"],
-	},
+  test: {
+    include: ["src/**/*.test.ts"],
+    exclude: ["src/**/*-e2e.test.ts", "node_modules/**"],
+    passWithNoTests: true,
+  },
 });
